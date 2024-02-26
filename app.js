b1476f179bd4502f6d591e5c5d7040c1d369fe48
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-26 12:20:30 +0430

    adding some changes

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
  function Counter(element, value){
-
+    console.log(element, value)
  }
 
- const firstCounter = new Counter()
+ const firstCounter = new Counter(getElement(".first-counter"), 100);
+ const secondCounter = new Counter(getElement(".second-counter"), 200);
 
  function getElement (selection){
     const element = document.querySelector(selection)
+    if(element){
+        return element;
+    }
+    throw new Error(
+        `please check "${selection}" selector , no such element exisits`
+    );
  }
+
+//  getElement(".first-counter")
