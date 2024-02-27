b6bbb83ffe37dc7895fcc2615f93f923a2c8ed57
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-27 15:52:37 +0430

    adding some changes

---
+++ app-class.js
@@ -0,0 +1,16 @@
+   function getElement (selection){
+    const element = document.querySelector(selection)
+    if(element){
+        return element;
+    }
+    throw new Error(
+        `please check "${selection}" selector , no such element exisits`
+    );
+ };
+
+
+
+ 
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
